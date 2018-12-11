dbf7793a2253507552793338ade3a703bf65d97a
Caumes Kirian <kirian.caumes@nextdecision.fr>
2018-12-11 09:50:30 +0100

    start routing to get datas users

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const app = require('express')();
 // Set templating engine
 app.engine('html', function (filePath, options, callback) {
     fs.readFile(filePath, function (err, content) {
-        if(err) return callback(err);
-        return callback(null, require('mustache').to_html(content.toString(),options))
+        if (err) return callback(err);
+        return callback(null, require('mustache').to_html(content.toString(), options))
     });
 });
 
 app.set('views', __dirname + '/views');
-app.set('view engine','html');
+app.set('view engine', 'html');
 
 const utils = require('./utils');
 const models = require('./models');
@@ -38,6 +38,71 @@ app.all('admin', (req, res) => {
 
 // ************************************************************************************************
 
+
+// * NOSQL ROUTES *********************************************************************************
+app.all('/teacher/:id', (req, res) => {
+
+})
+app.all('/class/:id', (req, res) => {
+
+})
+app.all('/subject/:id', (req, res) => {
+
+})
+app.all('/student/:id', (req, res) => {
+    console.log("OUI", req.query);
+    models.user.findById(
+        req.params.id,
+        (error, user) => {
+            if (error) {
+                console.error(error)
+                res.status(500).send("something bad happend :'(")
+            } else {
+                models.mark.find({
+                    student: user._id
+                },
+                    (error, marks) => {
+                        if (error) {
+                            console.error(error)
+                            res.status(500).send("something bad happend :'(")
+                        } else {
+                            res.render('student.html', { user: user, marks: marks })
+                            //res.send(data)  
+                        }
+                    })
+            }
+        }
+    );
+    // let newUser = new models.user({
+    //     first_name: "Kirian",
+    //     name: "Caumes",
+    //     pwd: "oui",
+    //     mail: "kiki@mail.com",
+    //     roles: ["student"]
+    // });
+    // newUser.save((error) => {
+    //     if (error) console.error(error);
+    //     else models.user.findById(
+    //         req.params.id,
+    //         (error, data) => {
+    //             if (error) console.error(error)
+    //             else res.send(data);
+    //         }
+    //     );
+    // })
+
+    // models.user.find({},
+    //     (error, data) => {
+    //         if (error) console.error(error)
+    //         else res.send(data);
+    //     }
+    // );
+
+});
+
+
+// ************************************************************************************************
+
 // Run app
 app.listen(config.get('server.port'), () => {
     console.log(
@@ -48,7 +113,7 @@ app.listen(config.get('server.port'), () => {
 
 // Connect to MongoDB database with mongoose
 if (config.get('mongoose.url') !== '') {
-    mongoose.connect(config.get('mongoose.url'), {useNewUrlParser: true})
+    mongoose.connect(config.get('mongoose.url'), { useNewUrlParser: true })
         .then(
             () => console.log('Connected to database!'),
             err => console.log('Unable to connect to database...\n', err)
